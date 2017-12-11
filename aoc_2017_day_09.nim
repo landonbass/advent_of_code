9fb1d12e61d587c7b2269d42371d436724f54320
landon bass <landon@labtechnologygroup.com>
2017-12-11 09:21:19 -0500

    2017 day 09 part 2 completed

--- aoc_2017_day_09.nim
+++ aoc_2017_day_09.nim
@@ -3,31 +3,42 @@ import memfiles
 var source = newSeq[string]()
 for line in lines(memfiles.open("aoc_2017_day_09.dat")):
     source.add line
-var scores    = newSeq[int]()
-var current   = 1
-var inGarbage = false
-var skipNext  = false
-for i in 0..source[0].len - 1:
-    if skipNext:
-        skipNext = false
-        continue
-    let ch = source[0][i]
-    if not inGarbage:
-        if ch == '{':
-            scores.add current
-            inc current
-        if ch == '}':
-            dec current
-        if ch == '<':
-            inGarbage = true
-    else:
-        if ch == '<':
-            inGarbage = true
-        if ch == '>':
-            inGarbage = false
-        if ch == '!':
-            skipNext = true    
 
-var sum : int = 0
-for score in scores: sum += score
-echo sum
+for line in source:
+    var scores    = newSeq[int]()
+    var current   = 1
+    var inGarbage = false
+    var skipNext  = false
+    var canceled  = 0
+    for i in 0..line.len - 1:
+        if skipNext:
+            skipNext = false
+            continue
+        let ch = line[i]
+        if not inGarbage:
+            if ch == '{':
+                scores.add current
+                inc current
+                continue
+            if ch == '}':
+                dec current
+                continue
+            if ch == '<':
+                inGarbage = true
+                continue
+        else:
+            if ch == '<':
+                inc canceled
+                continue
+            elif ch == '>':
+                inGarbage = false
+                continue
+            elif ch == '!':
+                skipNext = true 
+                continue
+            else:
+                inc canceled  
+
+    var sum : int = 0
+    for score in scores: sum += score
+    echo $sum & " canceled:" & $canceled
