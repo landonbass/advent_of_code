a3a2593247b9410b5b0d5aa58e49b70485dc5640
landon bass <landon@labtechnologygroup.com>
2017-12-15 08:41:45 -0500

    2017 day 15 part 2 completed

--- aoc_2017_day_15.nim
+++ aoc_2017_day_15.nim
@@ -11,9 +11,17 @@ type Iteration = object
     b : int
     r : bool
 
+proc genNextValue(p, f, m: int): int =
+    result = low int
+    var dp = (p * f).int mod divisor
+    while dp mod m != 0:
+        dp = (dp * f).int mod divisor
+        
+    result = dp
+
 proc genValues(iteration: Iteration) : Iteration =
-    let genA  = (iteration.a * factorA).int mod divisor
-    let genB  = (iteration.b * factorB).int mod divisor
+    let genA  = genNextValue(iteration.a, factorA, 4)
+    let genB  = genNextValue(iteration.b, factorB, 8)
     let binA  = toBin(genA, 32)[16..31]
     let binB  = toBin(genB, 32)[16..31]
     let match = if binA == binB: true else: false
@@ -21,11 +29,11 @@ proc genValues(iteration: Iteration) : Iteration =
 
 var total = 0
 var i : Iteration
-for x in 0..40_000_000:
+for x in 0..5_000_000:
     if x == 0:
         i.a = startA
         i.b = startB
-    i = genValues(i)
+    i = genValues i
     if i.r: inc total
 
 echo total
