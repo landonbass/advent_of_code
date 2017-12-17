0420522f154e2bf2ff7293d0a5315c619b71c7a8
landon bass <landon@labtechnologygroup.com>
2017-12-17 14:22:23 -0500

    2017 day 16 part 1 completed

---
+++ aoc_2017_day_16.nim
@@ -0,0 +1,66 @@
+import memfiles, sequtils, strutils
+
+type 
+    StepKind = enum 
+        spin,
+        exchange,
+        partner
+    Step = object 
+        case kind: StepKind
+        of spin:
+            X : int
+        of exchange:
+            eA, eB : int
+        of partner:
+            pA, pB : string
+
+var steps = newSeq[Step]()
+for line in lines(memfiles.open("aoc_2017_day_16.dat")):
+    let atoms = split(line, ",")
+    for atom in atoms:
+        let stepType = atom[0]
+        case stepType:
+            of 's':
+                steps.add Step(kind: spin, X: parseInt(atom[1..atom.len - 1]))
+            of 'x':
+                let d = find(atom, "/")
+                let a = parseInt(atom[1..d - 1])
+                let b = parseInt(atom[d + 1..atom.len - 1])
+                steps.add Step(kind: exchange, eA: a, eB: b)
+            of 'p':
+                let d = find(atom, "/")
+                let a = atom[1..d - 1] 
+                let b = atom[d + 1..atom.len - 1]
+                steps.add Step(kind: partner, pA: a, pB: b)
+            else: discard atom
+
+var programs = newSeq[string]()
+for i in 0..15:
+    programs.add $(i + 97).char
+
+for step in steps:
+    case step.kind:
+        of spin:
+            let a = programs[programs.len - step.X..programs.len - 1]
+            let b = programs[0..programs.len - 1 - step.X]
+            let newPrograms = concat(a, b)
+            for i in 0..newPrograms.len - 1:
+                programs[i] = newPrograms[i]    
+        of exchange:
+            let a = programs[step.eA]
+            let b = programs[step.eB]
+            programs[step.eA] = b
+            programs[step.eB] = a
+        of partner:
+            let ia = find(programs, step.pA)
+            let ib = find(programs, step.pB)
+            let a = programs[ia]
+            let b = programs[ib]
+            programs[ia] = b
+            programs[ib] = a
+
+var answer1 = ""
+for program in programs:
+    answer1 &= program
+
+echo answer1
