391f4779d72ff88b108d9fb046e850f4d39b5123
landon bass <landon@labtechnologygroup.com>
2017-12-18 16:25:22 -0500

    2017 day 16 part 2 completed

--- aoc_2017_day_16.nim
+++ aoc_2017_day_16.nim
@@ -34,33 +34,49 @@ for line in lines(memfiles.open("aoc_2017_day_16.dat")):
                 steps.add Step(kind: partner, pA: a, pB: b)
             else: discard atom
 
-var programs = newSeq[string]()
+var programMaster = newSeq[string]()
 for i in 0..15:
-    programs.add $(i + 97).char
+    programMaster.add $(i + 97).char
 
-for step in steps:
-    case step.kind:
-        of spin:
-            let a = programs[programs.len - step.X..programs.len - 1]
-            let b = programs[0..programs.len - 1 - step.X]
-            let newPrograms = concat(a, b)
-            for i in 0..newPrograms.len - 1:
-                programs[i] = newPrograms[i]    
-        of exchange:
-            let a = programs[step.eA]
-            let b = programs[step.eB]
-            programs[step.eA] = b
-            programs[step.eB] = a
-        of partner:
-            let ia = find(programs, step.pA)
-            let ib = find(programs, step.pB)
-            let a = programs[ia]
-            let b = programs[ib]
-            programs[ia] = b
-            programs[ib] = a
+var programs = programMaster 
 
+proc doSteps() =
+    for step in steps:
+        case step.kind:
+            of spin:
+                let a = programs[programs.len - step.X..programs.len - 1]
+                let b = programs[0..programs.len - 1 - step.X]
+                let newPrograms = concat(a, b)
+                for i in 0..newPrograms.len - 1:
+                    programs[i] = newPrograms[i]    
+            of exchange:
+                let a = programs[step.eA]
+                let b = programs[step.eB]
+                programs[step.eA] = b
+                programs[step.eB] = a
+            of partner:
+                let ia = find(programs, step.pA)
+                let ib = find(programs, step.pB)
+                let a = programs[ia]
+                let b = programs[ib]
+                programs[ia] = b
+                programs[ib] = a
+
+doSteps()
 var answer1 = ""
 for program in programs:
     answer1 &= program
-
 echo answer1
+
+var repeat = 0
+programs   = programMaster
+var first  = programs
+for i in 1..<1_000:
+    doSteps()
+    if first == programs: repeat = i; break
+programs = programMaster
+for i in 1..(1_000_000_000 mod repeat): doSteps()
+var answer2 = ""
+for program in programs:
+    answer2 &= program
+echo answer2
